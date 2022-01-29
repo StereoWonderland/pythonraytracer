09e0044fe50bc5a6ed8acf95cf7d270424dc1017
Mainthan Suriyakumaran <mainthan2@gmail.com>
2022-01-29 19:22:07 +0000

    Add surface module

---
+++ surface.py
@@ -0,0 +1,21 @@
+import numpy as np
+from abc import ABC, abstractmethod
+from ray import Ray
+
+class Surface(ABC):
+    @abstractmethod
+    def hit(self, ray: Ray) -> bool:
+        pass
+
+class Sphere(Surface):
+    def __init__(self, center: np.ndarray, radius: float):
+        self.center = center
+        self.radius = radius
+
+    def hit(self, ray: Ray) -> bool:
+        oc = ray.origin - self.center
+        a = np.dot(ray.direction, ray.direction)
+        b = 2 * np.dot(oc, ray.direction)
+        c = np.dot(oc, oc) - (radius ** 2)
+        disc = b ** 2 - 4 * a * c
+        return disc > 0
