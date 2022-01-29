1014ed14647326a24a44b3cde6dc3a60390743f2
Mainthan Suriyakumaran <mainthan2@gmail.com>
2022-01-29 20:44:48 +0000

    Increase value of t by 0.5 in ray_colour

--- render.py
+++ render.py
@@ -25,5 +25,5 @@ def ray_colour(ray: Ray, world: World) -> np.ndarray:
     if world.hit(ray):
         return np.array([255, 0, 0])
     else:
-        t = 0.5 * (ray.unit_direction()[1])
+        t = 0.5 * (ray.unit_direction()[1] + 1)
         return (1 - t) * np.array([255, 255, 255]) + t * np.array([130, 200, 255])
