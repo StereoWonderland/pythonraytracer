77bd35605cbe80686a7501046e8d189669375a2c
Mainthan Suriyakumaran <mainthan2@gmail.com>
2022-01-30 16:53:03 +0000

    Add Camera class to render module

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from render import Renderer
+from render import Camera, Renderer
 import surface
 
 IMAGE_WIDTH = 400
@@ -10,11 +10,13 @@ FOCAL_LENGTH = 1.0
 ORIGIN = np.array([0., 0., 0.])
 
 def main():
-    renderer = Renderer(IMAGE_WIDTH, IMAGE_HEIGHT, VIEWPORT_WIDTH,
+    camera = Camera(IMAGE_WIDTH, IMAGE_HEIGHT, VIEWPORT_WIDTH,
                         VIEWPORT_HEIGHT, FOCAL_LENGTH, ORIGIN)
+    renderer = Renderer(camera)
 
     world = surface.World()
     world.add(surface.Sphere(np.array([0., 0., -1.]), 0.5))
+    world.add(surface.Sphere(np.array([0., -100.5, -1]), 100))
 
     renderer.render(world)
     renderer.save('test1.png')
--- render.py
+++ render.py
@@ -3,7 +3,7 @@ from PIL import Image
 from ray import Ray
 from surface import World
 
-class Renderer:
+class Camera:
     def __init__(self, image_width, image_height, viewport_width,
                  viewport_height, focal_length, origin):
         self.image_width = image_width
@@ -13,7 +13,6 @@ class Renderer:
         self.focal_length = focal_length
         self.origin = origin
 
-        self.image = np.zeros((image_height, image_width, 3), dtype=np.uint8)
         self.horizontal = np.array([viewport_width, 0., 0.])
         self.vertical = np.array([0., viewport_height, 0.])
         self.lower_left = (self.origin
@@ -21,16 +20,24 @@ class Renderer:
                                   - self.vertical / 2
                                   - np.array([0., 0., self.focal_length]))
 
+    def get_ray(self, i, j):
+        x = j / (self.image_width - 1)
+        y = (self.image_height - 1 - i) / (self.image_height - 1)
+        ray = Ray(self.origin,
+                  self.lower_left + x * self.horizontal + y * self.vertical)
+        return ray
+
+class Renderer:
+    def __init__(self, camera: Camera):
+        self.camera = camera
+        self.image = np.zeros((camera.image_height, camera.image_width, 3),
+                              dtype=np.uint8)
+
     def render(self, world: World) -> None:
-        for i in range(self.image_height):
-            print(f'Scanlines remaining: {self.image_width - i}', end='\r')
-            for j in range(self.image_width):
-                x = j / (self.image_width - 1)
-                y = (self.image_height - 1 - i) / (self.image_height - 1)
-                ray = Ray(self.origin,
-                          self.lower_left
-                          + x * self.horizontal
-                          + y * self.vertical)
+        for i in range(self.camera.image_height):
+            print(f'Scanlines remaining: {self.camera.image_width - i}', end='\r')
+            for j in range(self.camera.image_width):
+                ray = self.camera.get_ray(i, j)
                 colour = ray_colour(ray, world)
                 self.image[i,j] = colour
 
