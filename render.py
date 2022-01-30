65343675c3a333d12d52746c052ed2f83f26a4ee
Mainthan Suriyakumaran <mainthan2@gmail.com>
2022-01-30 19:14:52 +0000

    Add diffuse shading

--- render.py
+++ render.py
@@ -29,9 +29,10 @@ class Camera:
         return ray
 
 class Renderer:
-    def __init__(self, camera: Camera, samples: int = 1):
+    def __init__(self, camera: Camera, samples: int = 1, max_bounces: int = 50):
         self.camera = camera
         self.samples = samples
+        self.max_bounces = max_bounces
         self.image = np.zeros((camera.image_height, camera.image_width, 3),
                               dtype=np.uint8)
 
@@ -43,19 +44,29 @@ class Renderer:
                 colour = np.array([0., 0., 0.,])
                 for _ in range(self.samples):
                     ray = self.camera.get_ray(i + random(), j + random())
-                    colour += ray_colour(ray, world)
+                    colour += ray_colour(ray, world, self.max_bounces)
                 self.image[i,j] = colour / self.samples
 
     def save(self, file_name: str) -> None:
         img = Image.fromarray(self.image, 'RGB')
         img.save(file_name)
 
-def ray_colour(ray: Ray, world: World) -> np.ndarray:
+def ray_colour(ray: Ray, world: World, depth: int) -> np.ndarray:
+    if depth <= 0:
+        return np.array([0, 0, 0])
     hit_data = world.hit(ray)
     if hit_data.time > 0:
-        return 0.5 * 255 * np.array([hit_data.normal[0] + 1,
-                                     hit_data.normal[1] + 1,
-                                     hit_data.normal[2] + 1])
+        target = hit_data.point + hit_data.normal + random_point_in_unit_ball()
+        return 0.5 * ray_colour(Ray(hit_data.point,
+                                    np.subtract(target, hit_data.point)),
+                                world, depth - 1)
     else:
         t = 0.5 * (ray.unit_direction()[1] + 1)
         return (1 - t) * np.array([255, 255, 255]) + t * np.array([130, 200, 255])
+
+def random_point_in_unit_ball() -> np.ndarray:
+    random_point = np.random.uniform(-1, 1, (3,))
+    if np.sum(np.square(random_point)) <= 1:
+        return random_point
+    else:
+        return random_point_in_unit_ball()
